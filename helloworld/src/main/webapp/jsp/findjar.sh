a17ccf701ed0d7b7f78bfc1761f134f2b8fe3384
jangminjun <38595676+jangminjun@users.noreply.github.com>
2021-04-07 16:21:44 +0900

    Delete helloworld/src/main/webapp/jsp directory

--- helloworld/src/main/webapp/jsp/findjar.sh
+++
@@ -1,8 +0,0 @@
-LIST=`find ./* -name "*.jar" -print | awk '{ print $0 }'`
-for JAR in ${LIST}
-do
-echo "############################################################"
-echo " "${PWD}/$JAR
-echo "------------------------------------------------------------"
-jar tvf $JAR | awk '{ print $8 }' | grep $1
-done
