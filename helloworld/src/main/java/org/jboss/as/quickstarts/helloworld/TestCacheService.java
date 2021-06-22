e276d57b5fa25e4b92d0c77c95e70eb373cd3422
jangminjun <38595676+jangminjun@users.noreply.github.com>
2021-06-22 17:15:42 +0900

    Create TestCacheService.java

---
+++ helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/TestCacheService.java
@@ -0,0 +1,16 @@
+@Named
+public class TestCacheService {
+
+    public static final String REMOTE_CACHE_NAME = "myremotecache";
+
+    @Resource(lookup = "java:jboss/infinispan/remote-container/myRemoteContainer")
+    org.infinispan.client.hotrod.RemoteCacheContainer remoteCacheContainer;
+
+    public void putRemoteCache(String key, String value) {
+        remoteCacheContainer.getCache(REMOTE_CACHE_NAME).put(key, String.format("%s (%s)", value, new Date()));
+    }
+
+    public Object getRemoteCache(String key) {
+        return remoteCacheContainer.getCache(REMOTE_CACHE_NAME).get(key);
+    }
+}
